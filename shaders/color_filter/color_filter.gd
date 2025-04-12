76672541220f75fd2bbcca564a62ea7cd80c00b3
Torben Van Assche <torbenvanassche.anime@gmail.com>
2025-04-12 19:50:47 +0200

    added some meshes and level design, shader fixes

--- shaders/color_filter/color_filter.gd
+++ shaders/color_filter/color_filter.gd
@@ -3,9 +3,16 @@ extends Node
 @export var colors: Array[Color] = [Color("#ac432f")]
 @export var shaders: Array[ShaderMaterial] = [];
 
-func _ready() -> void:	
-	#colors = FileUtils.get_colors_from_palette(shader_material.get_shader_parameter("albedo_texture"), 4, 8);
+func _ready() -> void:
+	for shader in shaders:
+		shader.set_shader_parameter("use_emission", shader.get_shader_parameter("emission_texture"))
+		shader.set_shader_parameter("force_texture", false)
+		shader.set_shader_parameter("use_emission", false)
 	apply_palette_to_shader()
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed(&"switch_material_mode"): 
+		toggle_textures()
 
 func apply_palette_to_shader() -> void:
 	for shader in shaders:
@@ -15,3 +22,4 @@ func apply_palette_to_shader() -> void:
 func toggle_textures() -> void:
 	for shader in shaders:
 		shader.set_shader_parameter("force_texture", !shader.get_shader_parameter("force_texture"))
+		shader.set_shader_parameter("use_emission", shader.get_shader_parameter("emission_texture") && shader.get_shader_parameter("force_texture"))
